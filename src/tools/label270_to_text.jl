59327ebf60e2161c485385d05f05c5e10d72c0ac
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2017-07-11 22:56:20 -0700

    update label270_text to latest save format

--- src/tools/label270_to_text.jl
+++ src/tools/label270_to_text.jl
@@ -38,38 +38,26 @@ export label270_to_text
 
 using ..DefineSave
 using ..SaveHelpers
+using RLESUtils, ArrayUtils
 
-label270_to_text{T<:AbstractString}(infiles::Vector{T}) = pmap(label270_to_text, infiles)
-
+label270_to_text{T<:AbstractString}(infiles::Vector{T}) = map(label270_to_text, infiles)
 function label270_to_text(infile::AbstractString)
-  d = trajLoad(infile)
+    d = trajLoad(infile)
 
-  tind = sv_lookup_id(d, "wm", "t")
-  lind = sv_lookup_id(d, "ra", "label270")
+    outfileroot = getSaveFileRoot(infile)
+    outfile = string(outfileroot, "_label270.txt")
+    f = open(outfile, "w")
 
-  outfileroot = getSaveFileRoot(infile)
-  outfile = string(outfileroot,"_label270.txt")
-  f = open(outfile,"w")
-
-  for i = 1:sv_num_aircraft(d, "ra")
-    println(f, "Aircraft $i")
-
-    label_tm1 = ""
-    times = sv_simlog_tdata_vid(d, "wm", i, tind)
-    labels = sv_simlog_tdata_vid(d, "ra", i, lind)
-    for (t, label_t) in zip(times, labels)
-
-      if label_t != label_tm1 #only output on change
-        println(f, "$(t) \t\t $(label_t)")
-      end
-
-      label_tm1 = label_t
+    for i = 1:get_num_aircraft(d)
+        println(f, "Aircraft $i")
+        v = d["CAS_$i"][:label270]
+        inds = consec_unique_inds(v)
+        for t in inds  
+            println(f, "$(t) \t\t $(v[t])")
+        end
+        println(f, "\n\n")
     end
-
-    println(f, "\n\n")
-  end
-
-  close(f)
+    close(f)
 end
 
 end #module
